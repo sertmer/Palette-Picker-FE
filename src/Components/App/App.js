bb5923610a13a8cac1efa0aa2429841e617857a1
sertmer <scottertmer@gmail.com>
2020-02-08 14:01:14 -0700

    Adjust html title of app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
-import './App.css';
+import './App.scss';
+import Menu from '../Menu/Menu';
+import PaletteContainer from '../PaletteContainer/PaletteContainer';
+import Folder from '../Folder/Folder';
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <header className="App-header">
         <h1>hey</h1>
       </header>
+      <Menu />
+      <PaletteContainer />
+      <Folder />
     </div>
   );
 }
