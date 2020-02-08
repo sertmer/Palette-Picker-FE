9351468e4ee78c43c071114afca5461a6fb77d2c
sertmer <scottertmer@gmail.com>
2020-02-08 14:36:33 -0700

    Build inital FoldersContainer

---
+++ src/Components/FoldersContainer/FoldersContainer.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import './FoldersContainer.scss';
+
+const FoldersContainer = () => {
+  return (
+    <h2>it's the folders container</h2>
+  )
+}
+
+export default FoldersContainer;
