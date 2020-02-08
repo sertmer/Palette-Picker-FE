e3ff84ef6e7a68e98f5ef8efe23d5abc7218894b
sertmer <scottertmer@gmail.com>
2020-02-08 14:02:13 -0700

    Build initial PaletteContainer component

---
+++ src/Components/PaletteContainer/PaletteContainer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './PaletteContainer.scss'
+import Color from '../Color/Color';
+
+const PaletteContainer = () => {
+  return (
+    <>
+    <h2>it's the palette container</h2>
+    <Color />
+    </>
+  )
+}
+
+export default PaletteContainer;
