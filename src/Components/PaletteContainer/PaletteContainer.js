0d777de5185d00bc4627d95d3b09b3f39f601c2e
sertmer <scottertmer@gmail.com>
2020-02-08 16:41:15 -0700

    Render color components through PalContainer

--- src/Components/PaletteContainer/PaletteContainer.js
+++ src/Components/PaletteContainer/PaletteContainer.js
@@ -2,10 +2,19 @@ import React, { useState } from 'react';
 import './PaletteContainer.scss'
 import Color from '../Color/Color';
 
+
 const PaletteContainer = () => {
+  const newColorGenerator = () => {
+    return Math.floor(Math.random()*16777215).toString(16);
+  }
+  let arr = [1, 2, 3, 4, 5];
+  let colorsToDisplay = arr.map((color, idx) => {
+    color = newColorGenerator()
+    return <Color key={idx} color={color} />
+  })
   return (
-    <section>
-      <h2> its the palette container</h2>
+    <section className='palette-container'>
+      {colorsToDisplay}
     </section>
   )
 }
