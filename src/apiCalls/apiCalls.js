7a791fb28e162dba04388d80c7cb8e2229bd70d6
James Miller <james.rex.miller.4@gmail.com>
2020-02-09 07:46:46 -0700

    Add getPalettes apiCall

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -16,3 +16,22 @@ export const getFolders = async () => {
       }
       return res.json()})
 }
+
+export const getPalettes = async (id) => {
+  let url = window.location.href;
+  url = url.split('/');
+
+  if (url[2] === 'localhost:3000') {
+    url[2] = 'localhost:3001'
+  }
+  url.splice(3, 0, 'api/v1');
+
+  url = url.join('/');
+
+  return fetch(url)
+    .then(res => {
+      if(!res.ok) {
+        throw Error('Failed fetching folders')
+      }
+      return res.json()})
+}
