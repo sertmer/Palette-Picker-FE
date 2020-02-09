98bf2b008c3bc7a11c257f4d66d0c3a208f50e43
James Miller <james.rex.miller.4@gmail.com>
2020-02-08 20:39:36 -0700

    Add useReducer, useContext, createContext to App and PaletteContainer

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,19 +5,52 @@ import NoURLMatch from '../NoURLMatch/NoURLMatch.test';
 import Main from '../Main/Main';
 import { Route, Switch } from 'react-router-dom';
 
+const newColorGenerator = () => {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+const defaultColors = [
+  {color: newColorGenerator(), locked: false},
+  {color: newColorGenerator(), locked: false},
+  {color: newColorGenerator(), locked: false},
+  {color: newColorGenerator(), locked: false},
+  {color: newColorGenerator(), locked: false},
+];
+const reducer = (state = defaultColors, action = {}) => {
+  switch (action.type) {
+    case 'GENERATE COLORS':
+      return action.payload.map(element => {
+        if (element.locked) {
+          return element
+        }
+        return element.color = newColorGenerator();
+      });
 
-function App() {
+    default:
+      return state;
+  };
+};
+
+export const ColorsContext = createContext({color: '3f63fd', locked: false});
+
+export const useStore = () => useContext(ColorsContext);
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, defaultColors)
+  const value = { state, dispatch }
+  
   return (
-    <main className="App">
-      <header className="App-header">
-        <h1>Palette Picker</h1>
-      </header>
-      <Switch>
-        <Route exact path='/' component={Main} />
-        <Route exact path='/folders' component={FoldersContainer} />
-        <Route component={NoURLMatch} />
-      </Switch>
-    </main>
+    <ColorsContext.Provider value={value}>
+      <main className="App">
+        <header className="App-header">
+          <h1>Palette Picker</h1>
+        </header>
+        <Switch>
+          <Route exact path='/' component={Main} />
+          <Route exact path='/folders' component={FoldersContainer} />
+          <Route component={NoURLMatch} />
+        </Switch>
+      </main>
+    </ColorsContext.Provider>
   );
 }
 
--- src/Components/Color/Color.js
+++ src/Components/Color/Color.js
@@ -4,7 +4,7 @@ import './Color.scss'
 const Color = ({color}) => {
   return (
     <div className='color-column' style={{backgroundColor: `#${color}`}}>
-      <h2 className='color-h2'>#{color.toUpperCase()}</h2>
+      <h2 className='color-h2'>#{color}</h2>
     </div>
   )
 }
