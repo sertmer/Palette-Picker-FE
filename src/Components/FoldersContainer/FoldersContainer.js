aa9fef3f887c128a9ee3623de4357036ff217409
James Miller <james.rex.miller.4@gmail.com>
2020-02-08 23:57:41 -0700

    Add apiCalls to get folders + useState and useEffect to render data

--- src/Components/FoldersContainer/FoldersContainer.js
+++ src/Components/FoldersContainer/FoldersContainer.js
@@ -1,9 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { getFolders } from '../../apiCalls/apiCalls';
+import folderIcon from '../../Images/003-folder.svg';
 import './FoldersContainer.scss';
 
 const FoldersContainer = () => {
+  const [folders, setFolders] = useState([]);
+
+  useEffect(() => {
+    getFolders()
+      .then(data => {
+        setFolders(data)
+      })
+      .catch(error => console.error(`Something went wrong ${error}`))
+  }, []);
+
+  let foldersArray = folders.map(folder => {
+      return (
+      <div className="folder-div">
+        <img src={folderIcon} alt='icon of a folder' key={folder.id} 
+        className='icon' id={folder.id} />
+        <h2 className="folder-name-h2">{folder.folder_name}</h2>
+      </div>
+      )
+    })
+  
   return (
-    <h2>it's the folders container</h2>
+    <div className="folders-container-div">
+      {foldersArray}
+    </div>
   )
 }
 
