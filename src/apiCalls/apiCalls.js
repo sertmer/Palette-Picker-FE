0bef7e679e9b903db8894a8d4f6dfa50780e8cec
James Miller <james.rex.miller.4@gmail.com>
2020-02-11 15:41:58 -0700

    Write patchPalette method

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -37,3 +37,34 @@ export const postFolder = (folder) => {
       return res.json()
     })
 }
+
+export const patchPalette = (palette, name, id) => {
+  let folderId = window.location.pathname.split('/')[2]
+  console.log(folderId)
+  let url = process.env.REACT_APP_BACKEND_URL + `/api/v1/folders/${folderId}/palettes/${id}`
+  const body = {
+    palette_name: name,
+    color_one: palette[0].color,
+    color_two: palette[1].color,
+    color_three: palette[2].color,
+    color_four: palette[3].color,
+    color_five: palette[4].color,
+    folder_id: folderId
+  }
+
+  const options = {
+    method: 'PATCH',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }
+
+  return fetch(url, options)
+    .then(res => {
+      if (!res.ok){
+        throw Error('Error posting folder')
+      }
+      return res.json()
+    })
+}
